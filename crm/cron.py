ac6b2011d84da7faefd21e2342d5148f740b56ee
chika-12 <markworship001@gmail.com>
2025-11-01 14:00:24 +0100

    crontab for updating product

--- crm/cron.py
+++ crm/cron.py
@@ -3,13 +3,16 @@ from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 from datetime import datetime
 
-logging.basicConfig(
-  filename="/tmp/crm_heartbeat_log.txt",
-  level=logging.INFO,
-  format="%(asctime)s - %(levelname)s - %(message)s"
-)
+
 
 def log_crm_heartbeat():
+
+  logging.basicConfig(
+    filename="/tmp/crm_heartbeat_log.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+  )
+
   url = "http://localhost:8000/graphql/"
   timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
   
@@ -32,5 +35,54 @@ def log_crm_heartbeat():
       logging.warning(f"{timestamp} CRM is not responding accordinly")
   except Exception as e:
     logging.error(f"Error: {e}")
+
+def update_low_stock():
+
+  logging.basicConfig(
+    filename="/tmp/low_stock_updates_log.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+  )
+  url = "http://localhost:8000/graphql/"
+  timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+
+  try:
+    
+    mutation = gql ("""
+      mutation{
+        updateLowStockProducts {
+          success
+          message
+          updatedProducts {
+            id
+            name
+            stock
+          }
+        }
+      }
+    """)
+
+    transport = RequestsHTTPTransport(
+      url=url,
+      verify=True,
+      retries=3
+    )
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    response = client.execute(mutation)
+    result = response["updateLowStockProducts"]
+
+    if result["success"] == True:
+      updated = result["updatedProducts"]
+      logging.info(f"{timestamp} {updated}, \n Updated {len(updated)} products successfully")
+
+    else:
+      logging.warning(f"{timestamp} No product was updated")
+
+  except Exception as e:
+    logging.error(f"{timestamp} Error: {e}")
+
+
+
+
 
 
--- crm/schema.py
+++ crm/schema.py
@@ -106,6 +106,39 @@ class CreateOrder(graphene.Mutation):
       message="Order(s) created successfully"
     )
 
+#Special class
+class UpdateLowStockProducts(graphene.Mutation):
+  class Arguments:
+    pass
+
+  updated_product = graphene.List(ProductType)
+  success = graphene.Boolean()
+  message = graphene.String()
+  
+  def mutate(self, info):
+
+    low_stock_product = Product.objects.filter(stock__lt=10)
+
+    if not low_stock_product:
+      return UpdateLowStockProducts(
+        updated_product=[],
+        success=False,
+        message="No low stock product found"
+      )
+
+    for product in low_stock_product:
+      product.stock += 10
+      product.save()
+    
+    return UpdateLowStockProducts(
+      updated_product=low_stock_product,
+      success=True,
+      message="Low product updated successfully"
+    )
+    
+
+    
+
 class Query(graphene.ObjectType):
   all_customers = graphene.List(CustomerType)
   all_product = graphene.List(ProductType)
@@ -139,5 +172,6 @@ class Mutation(graphene.ObjectType):
   #bulk_create_customers = BulkCreateCustomers.Field()
   create_product = CreateProduct.Field()
   create_order = CreateOrder.Field()
+  update_low_stock_product = UpdateLowStockProducts.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
